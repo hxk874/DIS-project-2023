e45e2ca73f7d0102bf51e97ffcb02ca6af1f998e
kxd956 <124887660+kxd956@users.noreply.github.com>
2023-06-06 15:29:39 +0200

    update

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify
-from . import db, app, UPLOAD_FOLDER
-from .models import Sample
+from . import app, UPLOAD_FOLDER, conn, cur
+#from .models import Sample
 import json
 from distutils.log import debug
 from fileinput import filename
@@ -16,8 +16,8 @@ views = Blueprint('views', __name__)
 
 
 placement = 'website/'+str(UPLOAD_FOLDER)
+#cols = ['UNIQUE_ID','TECTONIC SETTING', 'LOCATION', 'ROCK NAME','MATERIAL','ROCK TYPE', 'SIO2(WT%)', 'AL2O3(WT%)', 'CAO(WT%)', 'NA2O(WT%)', 'K2O(WT%)', 'FEO(WT%)', 'FE2O3(WT%)', 'FEOT(WT%)', 'MGO(WT%)', 'MNO(WT%)', 'P2O5(WT%)', 'LOI(WT%)']
 cols = ['UNIQUE_ID','TECTONIC SETTING', 'LOCATION', 'ROCK NAME','MATERIAL','ROCK TYPE', 'SIO2(WT%)', 'AL2O3(WT%)', 'CAO(WT%)', 'NA2O(WT%)', 'K2O(WT%)', 'FEO(WT%)', 'FE2O3(WT%)', 'FEOT(WT%)', 'MGO(WT%)', 'MNO(WT%)', 'P2O5(WT%)', 'LOI(WT%)']
-
 
 @views.route('/', methods=['GET', 'POST'])
 def uploadFile():
@@ -29,43 +29,49 @@ def uploadFile():
 		data_filename = secure_filename(f.filename)
 		
 		f.save(os.path.join(placement, data_filename))
-		
+		tablename = 'sample'
 		session['uploaded_data_file_path'] = os.path.join(placement, data_filename)
-		parseCSV(session['uploaded_data_file_path'])
-
+		cur.execute(f"CREATE TABLE {tablename} (id SERIAL PRIMARY KEY, unique_id INT, tectonic_set VARCHAR(150), location VARCHAR(1000), rock_name VARCHAR(150), material VARCHAR(150), rock_type VARCHAR(150), siO2 FLOAT, al2o3 FLOAT, caO FLOAT, na2O FLOAT, k2O FLOAT, feO FLOAT, fe2O3 FLOAT, feO_total FLOAT, mgO FLOAT, mnO FLOAT, p2O5 FLOAT, loss FLOAT);")
+		parseCSV(session['uploaded_data_file_path'],tablename)
+		os.remove(os.path.join(placement, data_filename)) # remove csv file from uploads folder 
 
 		return render_template('index2.html')
 	return render_template("index.html")
 
 
-def parseCSV(filePath):
+def parseCSV(filePath, tablename):
 	# CVS Column Names
-	cols = ['UNIQUE_ID','TECTONIC SETTING', 'LOCATION', 'ROCK NAME','MATERIAL','ROCK TYPE', 'SIO2(WT%)', 'AL2O3(WT%)', 'CAO(WT%)', 'NA2O(WT%)', 'K2O(WT%)', 'FEO(WT%)', 'FE2O3(WT%)', 'FEOT(WT%)', 'MGO(WT%)', 'MNO(WT%)', 'P2O5(WT%)', 'LOI(WT%)']
+	
 	# Use Pandas to parse the CSV file
 	csvData = pd.read_csv(filePath, usecols=cols, encoding='unicode_escape')
 	csvData.dropna(subset='UNIQUE_ID',axis=0, inplace=True)
     # Loop through the Rows
+	
 	for index, row in csvData.iterrows():
-		new_sample = Sample(unique_id = row['UNIQUE_ID'], 
-							tectonic_set = row['TECTONIC SETTING'],
-    						location = row['LOCATION'],
-    						rock_name = row['ROCK NAME'],
-							material = row['MATERIAL'],
-							rock_type = row['ROCK TYPE'],
-							siO2 = row['SIO2(WT%)'],
-							al2o3 = row['AL2O3(WT%)'],
-							caO = row['CAO(WT%)'],
-							na2O = row['NA2O(WT%)'],
-							k2O = row['K2O(WT%)'],
-							feO = row['FEO(WT%)'],
-							fe2O3 = row['FE2O3(WT%)'],
-							feO_total = row['FEOT(WT%)'],
-							mgO = row['MGO(WT%)'],
-							mnO = row['MNO(WT%)'],
-							p2O5 = row['P2O5(WT%)'],
-							loss = row['LOI(WT%)'])
-		db.session.add(new_sample)
-		db.session.commit()
+		sql = "INSERT INTO sample (unique_id, tectonic_set, location, rock_name, material, rock_type, siO2, al2o3, caO, na2O, k2O, feO, fe2O3, feO_total, mgO, mnO, p2O5, loss) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+		values = (
+				#tablename,
+				row['UNIQUE_ID'], 
+				row['TECTONIC SETTING'], 
+				row['LOCATION'],
+				row['ROCK NAME'], 
+				row['MATERIAL'], 
+				row['ROCK TYPE'], 
+				row['SIO2(WT%)'],
+				row['AL2O3(WT%)'], 
+				row['CAO(WT%)'],
+				row['NA2O(WT%)'],
+				row['K2O(WT%)'],
+				row['FEO(WT%)'],
+				row['FE2O3(WT%)'],
+				row['FEOT(WT%)'],
+				row['MGO(WT%)'],
+				row['MNO(WT%)'],
+				row['P2O5(WT%)'],
+				row['LOI(WT%)']
+				)
+		cur.execute(sql, values)
+		conn.commit()
 	return render_template("index.html")
 
 
